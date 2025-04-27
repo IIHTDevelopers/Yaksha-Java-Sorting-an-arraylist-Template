fcb8557171914c825833bb48614415a063f8e68b
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2025-04-27 19:23:36 +0530

    Update ArrayListSortingOperations.java

--- src/main/java/com/yaksha/assignment/ArrayListSortingOperations.java
+++ src/main/java/com/yaksha/assignment/ArrayListSortingOperations.java
@@ -6,23 +6,7 @@ import java.util.Collections;
 public class ArrayListSortingOperations {
 
 	public static void main(String[] args) {
-		// **Creating an ArrayList**
-		ArrayList<Integer> arrayList = new ArrayList<>();
-
-		// **Adding items to ArrayList**
-		arrayList.add(3);
-		arrayList.add(1);
-		arrayList.add(4);
-		arrayList.add(2);
-		System.out.println("ArrayList after adding items: " + arrayList);
-
-		// **Sorting ArrayList in ascending order**
-		Collections.sort(arrayList);
-		System.out.println("ArrayList after sorting in ascending order: " + arrayList);
-
-		// **Sorting ArrayList in descending order**
-		Collections.sort(arrayList, Collections.reverseOrder());
-		System.out.println("ArrayList after sorting in descending order: " + arrayList);
+		// Write your logic here:
 
 	}
 }
