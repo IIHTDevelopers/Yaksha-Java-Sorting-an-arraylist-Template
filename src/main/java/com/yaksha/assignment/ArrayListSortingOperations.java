d8019bbb94b4af1311957503daf984223e2a636d
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2025-04-27 19:27:28 +0530

    Update ArrayListSortingOperations.java

--- src/main/java/com/yaksha/assignment/ArrayListSortingOperations.java
+++ src/main/java/com/yaksha/assignment/ArrayListSortingOperations.java
@@ -6,7 +6,13 @@ import java.util.Collections;
 public class ArrayListSortingOperations {
 
 	public static void main(String[] args) {
-		// Write your logic here:
+		// **Creating an ArrayList**
+
+		// **Adding items to ArrayList**
+
+		// **Sorting ArrayList in ascending order**
+
+		// **Sorting ArrayList in descending order**
 
 	}
 }
